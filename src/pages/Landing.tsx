3d21398c7c0bab95ae4f1511047ba70f3270cbe3
gabrielsbu <gabrielmaiasbu@gmail.com>
2020-10-13 17:54:19 -0300

    map adicionado, conhecimento bibliotecas de mapeamento

--- src/pages/Landing.tsx
+++ src/pages/Landing.tsx
@@ -21,7 +21,7 @@ function Landing(){
             <span>Jaguaruana-CE</span>
             </div>
 
-            <Link to="/app" className="enter-app">
+            <Link to="/orfanatos" className="enter-app">
                 <FiArrowRight size="26" color="rgba(0,0,0, 0.6)"/>
             </Link>
         </div>
--- src/pages/OrphanageMap.tsx
+++ src/pages/OrphanageMap.tsx
@@ -1,8 +1,44 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import { FiPlus } from 'react-icons/fi';
+
+import { Map, TileLayer } from 'react-leaflet';
+import 'leaflet/dist/leaflet.css'
+
+import '../styles/pages/orphanage.css';
+import mapMarkerImg from '../images/logo.svg';
 
 function OrphanageMap(){
     return (
-        <h1>ola</h1>
+        <div id="page-map">
+            <aside>
+                <header>
+                    <img src={mapMarkerImg} alt="Happy"/>
+                    <h2>Escolha um orfanato na mapa</h2>
+                    <p>Muitas crianças estão esperando sua visita :)</p>
+                </header>
+
+                <footer>
+                    <strong>Jaguaruana</strong>
+                    <span>Ceará</span>
+                </footer>
+            </aside>
+
+            <Map 
+                center={[-4.8415535,-37.7896551]}
+                zoom={14}
+                style={{ width: '100%', height: '100%'}}
+            >
+                {/* <TileLayer url="https://a.tile.openstreetmap.org/{z}/{x}/{y}.png"/> */}
+                <TileLayer 
+                    url={`https://api.mapbox.com/styles/v1/mapbox/light-v10/tiles/256/{z}/{x}/{y}@2x?access_token=${process.env.REACT_APP_MAPBOX_TOKEN}`}
+                />
+            </Map>
+
+            <Link to="" className="create-orphanage">
+                <FiPlus size={32} color="#fff"/>
+            </Link>
+        </div>
     );
 }
 
