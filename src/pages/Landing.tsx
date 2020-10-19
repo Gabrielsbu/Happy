6ccc1bc9e122f5098d8bed68065da98e008080df
gabrielsbu <gabrielmaiasbu@gmail.com>
2020-10-19 19:13:09 -0300

    Commit 2

--- src/pages/Landing.tsx
+++ src/pages/Landing.tsx
@@ -21,7 +21,7 @@ function Landing(){
             <span>Jaguaruana-CE</span>
             </div>
 
-            <Link to="/orfanatos" className="enter-app">
+            <Link to="/app" className="enter-app">
                 <FiArrowRight size="26" color="rgba(0,0,0, 0.6)"/>
             </Link>
         </div>
--- src/routes.tsx
+++ src/routes.tsx
@@ -3,13 +3,18 @@ import { BrowserRouter, Switch, Route } from 'react-router-dom';
 
 import Landing from './pages/Landing';
 import OrphanageMap from './pages/OrphanageMap';
+import Orphanage from './pages/Orphanage';
+import CreateOrphanage from './pages/CreateOrphanage';
 
 function Routes(){
     return (
         <BrowserRouter>
             <Switch>
                 <Route path="/" exact component={Landing}/>
-                <Route path="/orfanatos" component={OrphanageMap}/>
+                <Route path="/app" component={OrphanageMap}/>
+                
+                <Route path="/orfanatos/create" component={CreateOrphanage}/>
+                <Route path="/orfanatos/:id" component={Orphanage}/>
             </Switch>
         </BrowserRouter>
     );
